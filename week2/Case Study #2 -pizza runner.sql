6977dd1420bbf6ccfb5bbfbc8dfbb87a876a933f
sruthi b <67575229+sruthiammu@users.noreply.github.com>
2022-12-15 11:16:24 +0530

    Update Case Study #2 -pizza runner.sql

--- week2/Case Study #2 -pizza runner.sql
+++ week2/Case Study #2 -pizza runner.sql
@@ -1,1 +1,17 @@
+This case study has LOTS of questions - they are broken up by area of focus including:
+
+Pizza Metrics
+Runner and Customer Experience
+Ingredient Optimisation
+Pricing and Ratings
+Bonus DML Challenges (DML = Data Manipulation Language)
+Each of the following case study questions can be answered using a single SQL statement.
+
+Again, there are many questions in this case study - please feel free to pick and choose which ones you’d like to try!
+-- 
+Before you start writing your SQL queries however - you might want to investigate the data, you may want to do 
+something with some of those null values and data types in the customer_orders and runner_orders tables!
+
+
+### A.Pizza Metrics
 
